754096b2ce34b824dd7283ace3af933a4b1dae5b
Sami <lolipop66611@gmail.com>
2022-11-10 21:19:51 +0100

    Added home page

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+// Import gif
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={[styles.title, styles.text]}>HomeParty</Text>
+      <div>
+        <Lottie options={defaultOptions} height={400} width={400} />
+      </div>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  // Styles that are unchanged from previous step are hidden for brevity.
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+  text: {
+    color: "#ffffff",
+  },
+  title: {
+    fontSize: 30,
   },
 });
