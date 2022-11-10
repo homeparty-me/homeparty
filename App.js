95ca33c0995ba7407829a987b3cb94f3babba9aa
Sami <lolipop66611@gmail.com>
2022-11-10 21:30:31 +0100

    Completed Homepage

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
-// Import gif
-
+import { StyleSheet, View, Text, Image } from "react-native";
+import partyPopper from "./assets/animations/62715-party-popper1.gif";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={[styles.title, styles.text]}>HomeParty</Text>
-      <div>
-        <Lottie options={defaultOptions} height={400} width={400} />
-      </div>
+      <View>
+        <Image source={partyPopper} alt="Party Popper!" style={styles.gif} />
+      </View>
     </View>
   );
 }
@@ -18,7 +17,7 @@ const styles = StyleSheet.create({
   // Styles that are unchanged from previous step are hidden for brevity.
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
+    backgroundColor: "#ffffff",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -27,9 +26,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   text: {
-    color: "#ffffff",
+    color: "#25292e",
   },
   title: {
     fontSize: 30,
+    marginTop: 0,
+    marginBottom: 50,
+    position: "absolute ",
+  },
+  gif: {
+    width: 200,
+    height: 200,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
